be149b324166c0648ab008a40b9b00447e5fd448
Aufar <aufaradifan06@gmail.com>
2026-05-15 19:42:33 +0700

    nambahin summary

--- pertemuan_9_komstat.R
+++ pertemuan_9_komstat.R
@@ -1,7 +1,20 @@
+#Aufar_Radifan_1314624044
+data_1<-mtcars
+summary(data_1)
+mean(data_1$mpg)
+
+#Nambahin summary
+summary(data_1)
 
 
-iris
-summary(mtcars)
-mean(mtcars$mpg)
 
-data<-data(mtcars)
+
+
+
+
+
+
+
+
+
+
